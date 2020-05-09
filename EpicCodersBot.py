7f34fab0cb11814a6dffa07206a41abbd3d434db
Gustavo Guerra <gustavolaguerra@ymail.com>
2020-05-09 14:20:34 -0300

    First push into the repository
    Main files used to fetch contest data from Codeforces API

---
+++ EpicCodersBot.py
@@ -0,0 +1,101 @@
+import logging
+import EpicCodersContestWatcher
+from botTelegramKey import TKN
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+# For now, I only want to deal with commands.		#
+# I'm not importing this stuff, but I might			#
+# need it in the future:							#
+# 													#
+# from telegram.ext import MessageHandler, Filters  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+helloPhrase = "Projeto EPIC CODERS (Universidade Federal de Itajubá) - Maratona de Programação\nDigite /comandos para ver a lista de comandos"
+
+helpPhrase = "Comandos disponíveis:\n/contest\n"
+
+
+
+def getActiveText():
+	
+	activeContestList = EpicCodersContestWatcher.activeContests()
+	
+	if len(activeContestList) == 0:
+		return "Não há contests ativos no momento.\n\n"
+	
+	if activeContestList[0] == "error":
+		return activeContestList[1]
+	
+	answerContent = ""
+	
+	for contestName in activeContestList:
+		answerContent += contestName
+		answerContent += "\n"
+	
+	return answerContent
+
+
+
+def getUpcomingText():
+	
+	upcomingContestList = EpicCodersContestWatcher.upcomingContests()
+	
+	if len(upcomingContestList) == 0:
+		return "Não há contests em breve.\n"
+	
+	if upcomingContestList[0] == "error":
+		return upcomingContestList[1]
+	
+	answerContent = "Em breve:\n"
+	
+	for contestName in upcomingContestList:
+		answerContent += contestName
+		answerContent += "\n"
+	
+	return answerContent
+
+
+
+def startMessage(update, context):
+	context.bot.send_message(
+		chat_id=update.effective_chat.id, text=helloPhrase)
+	return
+
+
+
+def contestMessage(update, context):
+	context.bot.send_message(
+		chat_id=update.effective_chat.id, text=getActiveText() + getUpcomingText())
+	return
+
+
+
+def commandsMessage(update, context):
+	context.bot.send_message(
+		chat_id=update.effective_chat.id, text=helpPhrase)
+	return
+
+
+
+updater = Updater(token=TKN, use_context=True)
+dispatcher = updater.dispatcher
+
+# some exceptions are only caught by this
+logging.basicConfig(
+	format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+	level = logging.INFO)
+
+startHandler = CommandHandler('start', startMessage)
+contestHandler = CommandHandler('contest', contestMessage)
+helpHandler = CommandHandler('comandos', commandsMessage)
+
+dispatcher.add_handler(startHandler)
+dispatcher.add_handler(contestHandler)
+dispatcher.add_handler(helpHandler)
+
+updater.start_polling()
+
+updater.idle()
+# properly handles Ctrl + C
---
+++ EpicCodersContestWatcher.py
@@ -0,0 +1,174 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as expectedCon
+
+driverCF = webdriver.Firefox('/usr/local/')
+apiRequestLink = "https://codeforces.com/api/contest.list?gym=false"
+
+# At least in my computer, the api request page
+# loads differently in different browsers.
+# It would be easier for me to fetch data with Chrome
+
+
+
+# [This should be the only function that interacts with Codeforces API]
+def requestCF ():
+	
+	# Super important function that queries Codeforces API
+	# and returns the contest list in JSON format
+	
+	global driverCF
+	driverCF.get(apiRequestLink)
+	
+	# waits at most 5 seconds for desired element to load
+	WebDriverWait(driverCF, 5).until(
+		expectedCon.presence_of_element_located((By.ID, 'rawdata-tab'))
+	)
+	
+	# change the tabs in the page
+	dataTabElement = driverCF.find_element_by_id('rawdata-tab')
+	dataTabElement.click()
+	
+	# waits at most 5 seconds for desired element to load
+	WebDriverWait(driverCF, 5).until(
+		expectedCon.presence_of_element_located((By.CLASS_NAME, 'data'))
+	)
+	
+	return driverCF.find_element_by_class_name('data').text
+
+
+
+def CodeforcesFail (CFmessage):
+	# [THIS IS UNTESTED] because I wasn't able to get a 'FAILED'
+	# Prints API 'FAILED' comment
+	return ("[ERRO] Codeforces: " + CFmessage)
+
+
+
+def getTime(timeInfo):
+
+	timeInfo *= -1
+	
+	if timeInfo < 0:
+		return
+	
+	daysToStart = int(timeInfo/(60*60*24))
+	timeInfo %= 60*60*24
+	
+	if daysToStart == 1:
+		dayString = str(daysToStart) + " dia, "
+	else:
+		dayString = str(daysToStart) + " dias, "
+	
+	
+	hoursToStart = int(timeInfo/(60*60))
+	timeInfo %= 60*60
+	
+	if hoursToStart == 1:
+		hourString = str(hoursToStart) + " hora, "
+	else:
+		hourString = str(hoursToStart) + " horas, "
+	
+	
+	minutesToStart = int(timeInfo/60)
+	
+	if minutesToStart == 1:
+		minuteString = str(minutesToStart) + " minutos"
+	else:
+		minuteString = str(minutesToStart) + " minuto"
+	
+	fullTimeString = ""
+	
+	if daysToStart > 0:
+		fullTimeString += dayString
+	
+	if hoursToStart > 0:
+		fullTimeString += hourString
+	
+	if minutesToStart > 0:
+		fullTimeString += minuteString
+	
+	return fullTimeString
+
+
+
+def activeContests():
+
+	# Checks for active contests
+	# Returns a list:
+	
+	# If it's first element is "error", comment is the second element
+	
+	apiReturn = requestCF()
+	
+	# json.loads() parses the information 
+	# into a python dictionary
+	CFReturn = json.loads(apiReturn)
+	
+	currentlyRunning = []
+	
+	if CFReturn["status"] == "FAILED":
+		currentlyRunning.append("error")
+		currentlyRunning.append(CodeforcesFail(CFReturn["comment"]))
+	
+	else :
+		
+		for contest in CFReturn["result"]:
+			
+			contestPhase = contest["phase"]
+			contestName = contest["name"]
+			
+			if contestPhase == "CODING":
+				currentlyRunning.append(contestName)
+			elif contestPhase != "BEFORE":
+				break
+	
+	return currentlyRunning
+
+
+
+def upcomingContests():
+	
+	# Checks most recent 4 upcoming contests
+	
+	apiReturn = requestCF()
+	
+	# json.loads() parses the information 
+	# into a python dictionary
+	CFReturn = json.loads(apiReturn)
+	
+	upcoming = []
+	
+	if CFReturn["status"] == "FAILED":
+		upcoming.append("error")
+		upcoming.append(CodeforcesFail(CFReturn["comment"]))
+	
+	else :
+		
+		upcomingListIndex = 0
+		
+		for contest in CFReturn["result"]:
+			
+			contestPhase = contest["phase"]
+			contestName = contest["name"]
+			
+			if (contestPhase == "BEFORE") and (len(upcoming) < 4):
+				upcoming.append(contestName + " começará em " +
+				getTime(contest["relativeTimeSeconds"]))
+				
+			elif contestPhase == "BEFORE":
+				upcoming[upcomingListIndex] = contestName + " começará em " + getTime(contest["relativeTimeSeconds"])
+				
+				upcomingListIndex += 1
+				
+				if upcomingListIndex == 4:
+					upcomingListIndex = 0
+				
+			else:
+				break
+	
+	
+	return upcoming
